3a40380d15d02bed75b5787a99f0a68572ae366e
SvetaShelk <sveta.shelk@gmail.com>
2024-09-26 13:30:18 +0300

    добавлено решение задач 2.2 (pick), 2.3 (omit)

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    let obj_new = {};
 
-};
+    for (let obj_entry of Object.entries(obj)) {
+        if (fields.includes(obj_entry[0],0)) {
+            obj_new[obj_entry[0]] = obj_entry[1];
+        }
+    }
+    return obj_new;
+ };
