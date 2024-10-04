8c8fa8e07d778395ebb7562f67a53f006acea999
SvetaShelk <sveta.shelk@gmail.com>
2024-10-04 16:27:55 +0300

    Добавлено решение задач 3 блока

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    if (!obj) {
+        return;
+    }
 
+    const reverse_obj = {};
+    for (const [key, value] of Object.entries(obj)) {
+        reverse_obj[value] = key;
+    }
+    return reverse_obj;
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === undefined) {
+        return string;
+    }
 
+    if (string == null || string.length == 0 || size == 0) {
+        return '';
+    }
+
+    let repeat_num=1;
+    let string_corr=string[0];
+
+    for (let i=1; i < string.length; i++) {
+        if (string[i] == string[i-1]) {
+            repeat_num++;
+        }
+        else {
+            repeat_num=1;
+        }
+
+        if (repeat_num <= size) {
+            string_corr += string[i];
+        }
+    }
+    return string_corr;
 }
