1ea4ae82e788e88e11de3a5c3f91bc43fdac2dc6
SvetaShelk <sveta.shelk@gmail.com>
2024-10-20 00:16:32 +0300

    добавлено решение задания 6.1

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,47 @@
-export default class SortableTable {
+import SortableTableV1 from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
+
+export default class SortableTable extends SortableTableV1 {
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
 
+    super(headersConfig, data);
+
+    const sortFieldId = sorted.id || this.headerConfig.find(item => item.sortable).id;
+    const sortOrder = sorted.order || 'asc';                    
+    this.sort(sortFieldId, sortOrder);
+    this.createListeners();
+  }
+
+  handleHeaderClick = (e) => {
+    const cellElement = e.target.closest("div[class=sortable-table__cell");
+
+    if (!cellElement) {
+      return;
+    }
+
+    if (cellElement.dataset.sortable == false) {
+      return;
+    }
+
+    const fieldId = cellElement.dataset.id;
+    const sortOrder = (cellElement.dataset.order == 'desc') ? 'asc' : 'desc';
+
+    this.sort(fieldId, sortOrder);
+  }
+  
+  createListeners() {
+    this.subElements.header.addEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  destroyListeners() {
+    this.subElements.header.removeEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  destroy() {
+    super.destroy();
+    this.destroyListeners();
   }
 }
+
