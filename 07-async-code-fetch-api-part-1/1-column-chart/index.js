c41c212ee6e2e641704d6e809a5e55527cc865e2
SvetaShelk <sveta.shelk@gmail.com>
2024-10-29 19:54:25 +0300

    добавлено решение задачи 7.1 и соответствующие изменения в решение задачи 4

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,31 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChartV1 from "../../04-oop-basic-intro-to-dom/1-column-chart/index.js";
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
+    constructor(props={}) {
+        super(props);
 
+        this.url = props.url;
+        /*this.update(props.range.from, props.range.to);*/
+    }
+
+    async update(dateFrom, dateTo) {
+        this.element.className = "column-chart column-chart_loading";
+
+        const data = await this.fetchData(dateFrom, dateTo);
+        super.update(Object.values(data));
+        
+        this.element.className = "column-chart";
+        return data;
+    }
+
+    async fetchData(dateFrom, dateTo) {
+        const url = new URL(this.url, BACKEND_URL);
+        url.searchParams.set('from', dateFrom.toISOString());
+        url.searchParams.set('to', dateTo.toISOString());
+
+        return await fetchJson(url);
+    }
 }
